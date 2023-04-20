bfd785cd37424ab141028cb991ec622b1ed99496
gansong <15579067806@163.com>
2023-04-20 17:29:28 +0800

    deme.js测试

---
+++ demo.js
@@ -0,0 +1,14 @@
+// console.log("hello");
+// console.log({ a: "hello" }.valueOf());
+// function onEvent(i) {
+//   console.log(i);
+// }
+for (let i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 5000);
+}
+
+console.log("1234");
+
+console.log("1234");
